4262bddcb91c764bd37bd2d05ee686d58bbf8d6c
mingmingmon <mingmingmon@naver.com>
2023-11-14 17:01:59 +0900

    add example mapping table, found @Builder error

---
+++ spring/src/main/java/umc/study/domain/mapping/MemberAgree.java
@@ -0,0 +1,24 @@
+package umc.study.domain.mapping;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+import umc.study.domain.enums.Gender;
+import umc.study.domain.enums.MemberStatus;
+import umc.study.domain.enums.SocialType;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberAgree extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+
+}
---
+++ spring/src/main/java/umc/study/domain/mapping/MemberMission.java
@@ -0,0 +1,26 @@
+package umc.study.domain.mapping;
+
+import lombok.*;
+import umc.study.domain.common.BaseEntity;
+import umc.study.domain.enums.Gender;
+import umc.study.domain.enums.MemberStatus;
+import umc.study.domain.enums.MissionStatus;
+import umc.study.domain.enums.SocialType;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Getter
+@Builder
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+public class MemberMission extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private MissionStatus status;
+}
