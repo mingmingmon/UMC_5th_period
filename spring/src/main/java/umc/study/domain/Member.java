dc8878dcb91544e43218f0077c63d6ad6049645d
mingmingmon <mingmingmon@naver.com>
2023-12-26 13:16:19 +0900

    make default possible

--- spring/src/main/java/umc/study/domain/Member.java
+++ spring/src/main/java/umc/study/domain/Member.java
@@ -1,6 +1,9 @@
 package umc.study.domain;
 
 import lombok.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.DynamicInsert;
+import org.hibernate.annotations.DynamicUpdate;
 import umc.study.domain.common.BaseEntity;
 import umc.study.domain.enums.Gender;
 import umc.study.domain.enums.MemberStatus;
@@ -18,6 +21,8 @@ import java.util.List;
 
 @Entity
 @Getter
+@DynamicUpdate
+@DynamicInsert
 @Builder
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
@@ -43,6 +48,7 @@ public class Member extends BaseEntity {
 
     private Date brith_date;
 
+    @ColumnDefault("0")
     private Integer point;
 
     @Column(nullable = false, columnDefinition = "VARCHAR(15)")
